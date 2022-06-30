f605dc475ea5360b6562b10eab0166ce948eb26a
bodrovis <bodrovis@protonmail.com>
2022-06-30 22:15:31 +0300

    bump

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,5 @@
-$redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+# frozen_string_literal: true
+
+# rubocop:disable Style/GlobalVars
+$redis = Redis.new(url: ENV.fetch('REDIS_URL', nil), ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+# rubocop:enable Style/GlobalVars
