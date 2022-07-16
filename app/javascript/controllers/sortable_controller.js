0897fdcea61aac8da5cf8ca42dee3bd688f31ec8
bodrovis <bodrovis@protonmail.com>
2022-07-16 18:51:09 +0300

    opacity

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -12,9 +12,18 @@ export default class extends Controller {
 
     new Sortable(this.element, {
       onEnd: async function(e) {
-        const response = await patch(`/admin/games/${gameId}/reorder_questions`, { body: JSON.stringify({"new_index": e.newIndex + 1, "old_index": e.oldIndex + 1}) })
-        if (!response.ok) {
-          throw new Error(`Can't reorder: HTTP ${response.statusCode}`)
+        try {
+          this.option('disabled', true)
+          this.el.parentNode.classList.add('opacity-50')
+          const response = await patch(`/admin/games/${gameId}/reorder_questions`, { body: JSON.stringify({"new_index": e.newIndex + 1, "old_index": e.oldIndex + 1}) })
+          if (!response.ok) {
+            throw new Error(`Can't reorder: HTTP ${response.statusCode}`)
+          }
+        } catch (e) {
+          console.error(e)
+        } finally {
+          this.option('disabled', false)
+          this.el.parentNode.classList.remove('opacity-50')
         }
       }
     })
