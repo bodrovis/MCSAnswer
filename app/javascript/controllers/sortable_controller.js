8ae3d079aee2f549e26c1059002bfbb04b60f725
bodrovis <bodrovis@protonmail.com>
2022-07-16 22:38:39 +0300

    update pos

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -6,34 +6,53 @@ export default class extends Controller {
   static values = {
     game: String
   }
+  static targets = [ 'item' ]
 
   connect() {
     const gameId = this.gameValue
 
     this.sortable = new Sortable(this.element, {
-      onEnd: async function(e) {
+      handle: ".js-sort-handle",
+      onEnd: async (e) => {
         try {
-          this.option('disabled', true)
-          this.el.parentNode.classList.add('opacity-50')
-          const response = await patch(`/admin/games/${gameId}/reorder_questions`, { body: JSON.stringify({"new_index": e.newIndex + 1, "old_index": e.oldIndex + 1}) })
+          this.disable()
+
+          const response = await patch(
+            `/admin/games/${gameId}/reorder_questions`,
+            {
+              body: JSON.stringify({"new_index": e.newIndex + 1, "old_index": e.oldIndex + 1})
+            }
+          )
+
           if (!response.ok) {
             throw new Error(`Can't reorder: HTTP ${response.statusCode}`)
           }
+
+          this.updatePositions()
         } catch (e) {
           console.error(e)
         } finally {
-          this.option('disabled', false)
-          this.el.parentNode.classList.remove('opacity-50')
+          this.enable()
         }
       }
     })
+
+    this.updatePositions()
   }
 
   disable() {
     this.sortable.option('disabled', true)
+    this.sortable.el.parentNode.classList.add('opacity-50')
   }
 
   enable() {
     this.sortable.option('disabled', false)
+    this.sortable.el.parentNode.classList.remove('opacity-50')
+  }
+
+  updatePositions() {
+    this.itemTargets.forEach((item, index) => {
+      item.querySelector('.js-position').innerText = index + 1
+    })
   }
 }
