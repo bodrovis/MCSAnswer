3cbd11b1de6419d347339eebab0f539180063200
bodrovis <bodrovis@protonmail.com>
2022-07-24 18:50:17 +0300

    cleanup

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,3 @@
 #   User.create name: "User #{i + 1}", email: "user#{i + 1}@example.com", password: 'P@$$w0rdS',
 #               password_confirmation: 'P@$$w0rdS'
 # end
-
-# PlayingTeam.all.find_each { |t| t.update place: nil, total_answered: 0 }
-# Answer.all.find_each { |a| a.update correct: false }
-Game.all.each do |g|
-
-  Game.reset_counters(g.id, :playing_teams)
-
-  end
